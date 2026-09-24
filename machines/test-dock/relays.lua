5a0ad4dd1b164d086efa009178421f4cc1abfc89
antrathent-sys <antrathent@gmail.com>
2026-09-24 21:36:01 +1000

    sync machines/test-dock/relays.lua 2026-09-24_21-36-00

--- machines/test-dock/relays.lua
+++ machines/test-dock/relays.lua
@@ -1,13 +1,13 @@
--- relay map for test-dock, from `depot probe map` 2026-09-24 20:41
+-- relay map for test-dock, from `depot probe map` 2026-09-24 21:35
 -- device: place | assemble | belt | pusher; on: what ON does
 return {
-  { relay = "redstone_relay_1", device = "none" },
-  { relay = "redstone_relay_2", device = "none" },
+  { relay = "redstone_relay_1", device = "belt", side = "B", on = "fills" },
+  { relay = "redstone_relay_2", device = "belt", side = "A", on = "fills" },
   { relay = "redstone_relay_5", device = "pusher", on = "up" },
-  { relay = "redstone_relay_6", device = "assemble", side = "A" },
   { relay = "redstone_relay_7", device = "pusher", side = "A", on = "up" },
-  { relay = "redstone_relay_8", device = "assemble", side = "B" },
   { relay = "redstone_relay_9", device = "pusher", side = "B", on = "up" },
   { relay = "redstone_relay_10", device = "place", side = "A", on = "places" },
   { relay = "redstone_relay_11", device = "place", side = "B", on = "places" },
+  { relay = "redstone_relay_12", device = "assemble", side = "A" },
+  { relay = "redstone_relay_13", device = "assemble", side = "B" },
 }
